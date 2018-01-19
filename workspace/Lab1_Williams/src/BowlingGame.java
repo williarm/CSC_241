018413cc69605ace24d1142596776480ad893b64
Bobby Williams <rwilliams@cardconnect.com>
2018-01-18 18:14:49 -0700

    Initial creation of Lab 1 project

---
+++ workspace/Lab1_Williams/src/BowlingGame.java
@@ -0,0 +1,66 @@
+public class BowlingGame {
+
+	private String name;
+	private int[][] scores = new int[10][3];
+	private int strikes;
+	private int spares;
+	private int frame;
+	private int shot;
+	private int gutterBalls;
+	private int score;
+	private static final char tempScoreValue = 'N';
+	private static final int maxNumFrames = 10;
+	private static final int maxNumShots = 3;
+	
+	public BowlingGame(String n){
+		name = n;
+		for(int i = 0; i < maxNumFrames; i++){
+			for(int j = 0; j < maxNumShots; j++){
+				scores[i][j] = 0;
+			}
+		}
+		strikes = 0;
+		spares = 0;
+		gutterBalls = 0;
+		score = 0;
+		frame = 0;
+		shot = 0;
+	}
+	
+	public boolean shot(int p){  //return false if the frame is over.
+		boolean nextShot = false;
+		if(p == 10){
+			//your code goes here!
+		}
+		else{
+			//your code goes here!
+		}
+		return nextShot;
+	}
+	public int getFrame(){
+		return frame;
+	}
+
+	public String toString(){
+		return name + "'s game after frame " + frame + ": Strikes " + strikes 
+				+ ", Spares " + spares + ", Gutter Balls " + gutterBalls + ", Score " + score;
+	}
+	
+	public void computeScoreFrame(){
+		int f = getFrame()-1;
+		if(f >=2){ //look two frames back
+			//your algorithm for the bonus points goes here.
+		}
+		else if(f == 1){ //look one frame back
+			//your algorithm for the bonus points goes here.
+		}
+		else{  //first frame
+			score += scores[f][0] + scores[f][1];
+		}
+	}
+	
+	public int getScore(){
+		return score;
+	}
+}
+
---
+++ workspace/Lab1_Williams/src/BowlingGameDriver.java
@@ -0,0 +1,30 @@
+import javax.swing.JOptionPane;
+
+public class BowlingGameDriver{
+	public static void main(String[] args) {
+		String n = JOptionPane.showInputDialog("Please enter a name for a bowler: ");
+		BowlingGame g = new BowlingGame(n);
+		int scores1[][] = { {5, 5}, {3, 7}, {0, 0}, {4, 6}, {10, 0}, {1, 9}, {10, 0},
+							{8, 1}, {0, 10}, {10, 0, 10}
+						  };
+		int scores2[][] = { {5, 5}, {3, 7}, {0, 0}, {4, 6}, {10, 0}, {1, 9}, {10, 0},
+							{8, 1}, {0, 10}, {0, 10, 10}
+			  			  };
+		int scores3[][] = { {2, 6}, {10, 0}, {10, 0}, {5, 5}, {2, 0}, {7, 1}, {10, 0}, 
+						    {10, 0}, {5, 5}, {10, 3, 5}
+						  };
+		int pins, i = 0, j;
+		while(g.getFrame() < 10){
+			j = 0;
+			do{
+				//pins = Integer.parseInt(JOptionPane.showInputDialog("Enter the pins knocked over for frame " + (g.getFrame()+1) + " :"));
+				pins = scores3[i][j];
+				j++;
+			}while(g.shot(pins));
+			i++;
+			g.computeScoreFrame();
+			JOptionPane.showMessageDialog(null, g);	
+		}
+		JOptionPane.showMessageDialog(null, "The bowler's score is: " + g.getScore());	
+	}
+}
