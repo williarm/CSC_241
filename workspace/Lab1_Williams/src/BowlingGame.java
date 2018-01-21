0e88f648ed7c5f00db7b69cb37a05b7441b633f9
williarm <robert.m.williams2@gmail.com>
2018-01-20 21:52:54 -0700

    Working Exercise 1

--- workspace/Lab1_Williams/src/BowlingGame.java
+++ workspace/Lab1_Williams/src/BowlingGame.java
@@ -9,14 +9,15 @@ public class BowlingGame {
 	private int shot;
 	private int gutterBalls;
 	private int score;
+	private boolean thirdShot = false;
 	private static final char tempScoreValue = 'N';
 	private static final int maxNumFrames = 10;
 	private static final int maxNumShots = 3;
-	
-	public BowlingGame(String n){
+
+	public BowlingGame(String n) {
 		name = n;
-		for(int i = 0; i < maxNumFrames; i++){
-			for(int j = 0; j < maxNumShots; j++){
+		for (int i = 0; i < maxNumFrames; i++) {
+			for (int j = 0; j < maxNumShots; j++) {
 				scores[i][j] = 0;
 			}
 		}
@@ -27,66 +28,87 @@ public class BowlingGame {
 		frame = 0;
 		shot = 0;
 	}
-	
-	public boolean shot(int p){  //return false if the frame is over.
-		/*
-		 * Rules: If the frame is 1-9, then a Strike will increment the frame.
-		 * If frame is 10, then a strike enables two additional shots
-		 * 
-		 * 
-		Scoring rules:
-		
-		Strikes: 10 points in the current frame plus score from next two rolls
-		
-		Spares: 10 points in the current frame plus score from next roll only
-		
-		For Example:
-		If a strike is bowled in frame 1, 10 points are awarded. Then, if the next two rolls are 2 
-		and 5, 7 points are added to frame 1, and 7 points are added to the resulting score from frame 1
-		for a total score of 24 points after frame 2.
-		
-		If a spare is bowled in frame 1, 10 points are awarded. Then, if the next roll is a 2,
-		2 points are added to frame 1, and 2 points are added to the resulting score from frame 1
-		for a total score of 14 points after frame 2.
-		 */
+
+	public boolean shot(int p) { // return false if the frame is over.
 		boolean nextShot = false;
-		if(p == 10){
-			if(shot == 0)
+		//boolean thirdShot = false;
+		if (p == 10) {
+			if (shot == 0) {
+				System.out.println("in main if, shot 0 - frame is " + frame + " shot " + shot + " pins " + p);
 				strikes++;
-			//your code goes here!
-			if(shot == 1)
+				if (frame == 9) {
+					nextShot = true;
+					thirdShot = true;
+				}
+			}
+			if (shot == 1) {
+				System.out.println("in main if, shot 0 - frame is " + frame + " shot " + shot + " pins " + p);
 				spares++;
+				if (frame == 9) {
+					nextShot = true;
+					thirdShot = true;
+				}
+			}
+			if (shot == 2) {
+				System.out.println("in main if, shot 0 - frame is " + frame + " shot " + shot + " pins " + p);
+				strikes++;
+				nextShot = false;
+				thirdShot = false;
+			}
+		} else {
+			if (p == 0) {
+				gutterBalls++;
+			}
+			if (shot == 0) {
+				System.out.println("in else, shot 0 - frame is " + frame + " shot " + shot + " pins " + p);
+				nextShot = true;
+			}
+			if (shot == 1) {
+				System.out.println("in else, shot 1 - frame is " + frame + " shot " + shot + " pins " + p);
+				if (frame == 9 && thirdShot) {
+					nextShot = true;
+				} else {
+					nextShot = false;
+				}
+			}
+			if (shot == 2) {
+				System.out.println("in else, shot 2 - frame is " + frame + " shot " + shot + " pins " + p);
+				nextShot = false;
+				thirdShot = false;
+			}
+
 		}
-		else{
-			//your code goes here!
+		if (nextShot||thirdShot) {
+			shot++;
+		} else {
+			frame++;
+			shot = 0;
 		}
-		frame++;
-		return nextShot;
+		System.out.println(nextShot || thirdShot);
+		return nextShot || thirdShot;
 	}
-	public int getFrame(){
+
+	public int getFrame() {
 		return frame;
 	}
 
-	public String toString(){
-		return name + "'s game after frame " + frame + ": Strikes " + strikes 
-				+ ", Spares " + spares + ", Gutter Balls " + gutterBalls + ", Score " + score;
+	public String toString() {
+		return name + "'s game after frame " + frame + ": Strikes " + strikes + ", Spares " + spares + ", Gutter Balls "
+				+ gutterBalls + ", Score " + score;
 	}
-	
-	public void computeScoreFrame(){
-		int f = getFrame()-1;
-		if(f >=2){ //look two frames back
-			//your algorithm for the bonus points goes here.
-		}
-		else if(f == 1){ //look one frame back
-			//your algorithm for the bonus points goes here.
-		}
-		else{  //first frame
+
+	public void computeScoreFrame() {
+		int f = getFrame() - 1;
+		if (f >= 2) { // look two frames back
+			// your algorithm for the bonus points goes here.
+		} else if (f == 1) { // look one frame back
+			// your algorithm for the bonus points goes here.
+		} else { // first frame
 			score += scores[f][0] + scores[f][1];
 		}
 	}
-	
-	public int getScore(){
+
+	public int getScore() {
 		return score;
 	}
 }
-
--- workspace/Lab1_Williams/src/BowlingGameDriver.java
+++ workspace/Lab1_Williams/src/BowlingGameDriver.java
@@ -14,15 +14,23 @@ public class BowlingGameDriver{
 			  			  };
 		int scores3[][] = { {2, 6}, {10, 0}, {10, 0}, {5, 5}, {2, 0}, {7, 1}, {10, 0}, 
 						    {10, 0}, {5, 5}, {10, 3, 5} //total 158
+		//int scores3[][] = {{10, 3, 5} }; // {2, 6}, {10, 0}, {10, 0}, {5, 5}, {2, 0}, {7, 1}, {10, 0}, 
+		//				    {10, 0}, {5, 5}, {10, 3, 5} //total 158
 						  };
 		int pins, i = 0, j;
 		while(g.getFrame() < 10){
 			j = 0;
 			do{
 				//pins = Integer.parseInt(JOptionPane.showInputDialog("Enter the pins knocked over for frame " + (g.getFrame()+1) + " :"));
+				
 				pins = scores1[i][j];
+				//System.out.println("i: "+i+" j: "+ j+" pins: "+pins);
+				
+				//System.out.println("i: "+i+" j: " + " pins: "+ pins +" frame: "+ (g.getFrame()));
+				//System.out.println(g.getFrame());
 				j++;
 			}while(g.shot(pins));
+			//System.out.println("another shot: "+ g.shot(pins));
 			i++;
 			//g.computeScoreFrame();
 			JOptionPane.showMessageDialog(null, g);	
