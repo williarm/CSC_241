37fcd4295e992797c26966f6902f41fb687eef0a
williarm <robert.m.williams2@gmail.com>
2018-01-27 08:55:11 -0700

    Updates after adding comments

--- workspace/Lab1_Williams/src/BowlingGame.java
+++ workspace/Lab1_Williams/src/BowlingGame.java
@@ -1,5 +1,18 @@
 import javax.swing.JOptionPane;
 
+/**
+ * Purpose: The BowlingGame program is designed to compute the score of a
+ * player's Bowling Game.
+ * 
+ * Constructors: BowlingGame
+ * 
+ * Methods: shot, getFrame, toString, computeScoreFrame, computScore, getScore
+ * 
+ * @author Robert Williams
+ * @since 2018-01-27
+ * 
+ */
+
 public class BowlingGame {
 
 	private String name;
@@ -10,12 +23,21 @@ public class BowlingGame {
 	private int shot;
 	private int gutterBalls;
 	private int score;
-	private int[] frameScores = new int[10];
 	private boolean thirdShot = false;
 	private static final char tempScoreValue = 'N';
 	private static final int maxNumFrames = 10;
 	private static final int maxNumShots = 3;
 
+	/**
+	 * Purpose: The BowlingGame constructor creates a BowlingGame object and
+	 * initializes the name, strikes, spares, gutterBalles, score, frame, and shot
+	 * variables.
+	 * 
+	 * @param n
+	 *            The name of the bowler.
+	 * @return nothing
+	 * 
+	 */
 	public BowlingGame(String n) {
 		name = n;
 		for (int i = 0; i < maxNumFrames; i++) {
@@ -31,33 +53,24 @@ public class BowlingGame {
 		shot = 0;
 	}
 
+	/**
+	 * Purpose: The shot method determines if the bowler has another shot left in
+	 * the frame.
+	 * 
+	 * Rules: If the frame is 1-9, then a Strike will increment the frame and the
+	 * bowler will not have another shot in the frame. If frame is 10, then a strike
+	 * on the first shot enables two additional shots. In frame 10, a spare on the
+	 * second shot will allow a third shot.
+	 * 
+	 * @param p
+	 *            The number of pins knocked down on the shot.
+	 * @return A boolean if the bowler has another shot.
+	 * 
+	 */
+
 	public boolean shot(int p) { // return false if the frame is over.
 		boolean nextShot = false;
 		scores[frame][shot] = p;
-		// thirdShot = (scores[9][0] == 10);
-		// System.out.println("frame: "+ frame + " shot: "+ shot + " scores: " +
-		// scores[frame][shot]);
-		/*
-		 * Rules: If the frame is 1-9, then a Strike will increment the frame. If frame
-		 * is 10, then a strike enables two additional shots. In frame 10, a spare on
-		 * the second shot will allow a third shot.
-		 * 
-		 * 
-		 * Scoring rules:
-		 * 
-		 * Strikes: 10 points in the current frame plus score from next two rolls
-		 * 
-		 * Spares: 10 points in the current frame plus score from next roll only
-		 * 
-		 * For Example: If a strike is bowled in frame 1, 10 points are awarded. Then,
-		 * if the next two rolls are 2 and 5, 7 points are added to frame 1, and 7
-		 * points are added to the resulting score from frame 1 for a total score of 24
-		 * points after frame 2.
-		 * 
-		 * If a spare is bowled in frame 1, 10 points are awarded. Then, if the next
-		 * roll is a 2, 2 points are added to frame 1, and 2 points are added to the
-		 * resulting score from frame 1 for a total score of 14 points after frame 2.
-		 */
 
 		if (p == 10) {
 			if (shot == 0) {
@@ -91,8 +104,7 @@ public class BowlingGame {
 			}
 			if (shot == 1) {
 
-				if ((scores[frame][shot - 1] != 10) && scores[frame][shot - 1] + p == 10) {// if scores[frame][shot-1]
-																							// !=10 prevShotPins
+				if ((scores[frame][shot - 1] != 10) && scores[frame][shot - 1] + p == 10) {
 					spares++;
 				}
 
@@ -103,8 +115,6 @@ public class BowlingGame {
 				}
 			}
 			if (shot == 2) {
-				// System.out.println("in else, shot 2 - frame is " + frame + " shot " + shot +
-				// " pins " + p);
 				nextShot = false;
 				thirdShot = false;
 			}
@@ -120,15 +130,53 @@ public class BowlingGame {
 		return nextShot || thirdShot;
 	}
 
+	/**
+	 * Purpose: The getFrame method is the getter method for the frame variable.
+	 * 
+	 * @param nothing
+	 * @return an int representing the value of the current frame
+	 * 
+	 */
 	public int getFrame() {
 		return frame;
 	}
 
+	/**
+	 * Purpose: The toString method returns a String message containing the bowler's
+	 * name, frame, strikes, spares, gutterBalls, and score after each frame.
+	 * 
+	 * @param nothing
+	 * @return a String message with attributes about the bowler's game
+	 * 
+	 */
 	public String toString() {
 		return name + "'s game after frame " + frame + ": Strikes " + strikes + ", Spares " + spares + ", Gutter Balls "
 				+ gutterBalls + ", Score " + score;
 	}
 
+	/**
+	 * Purpose: The computeScoreFrame method computes the bowler's score after each
+	 * frame.
+	 * 
+	 * Scoring Rules:
+	 * 
+	 * Strikes: 10 points in the current frame plus score from next two shots
+	 * 
+	 * Spares: 10 points in the current frame plus score from next shot only
+	 * 
+	 * For Example: If a strike is bowled in frame 1, 10 points are awarded. Then,
+	 * if the next two shots are 2 and 5, 7 points are added to frame 1, and 7
+	 * points are added to the resulting score from frame 1 for a total score of 24
+	 * points after frame 2.
+	 * 
+	 * If a spare is bowled in frame 1, 10 points are awarded. Then, if the next
+	 * shot is a 2, 2 points are added to frame 1, and 2 points are added to the
+	 * resulting score from frame 1 for a total score of 14 points after frame 2.
+	 * 
+	 * @param nothing
+	 * @return nothing
+	 * 
+	 */
 	public void computeScoreFrame() {
 		/*
 		 * Strikes: 10 points in the current frame plus score from next two rolls 10 +
@@ -145,8 +193,8 @@ public class BowlingGame {
 			// strike last 2 frames
 			if (scores[f - 2][0] == 10 && scores[f - 1][0] == 10) {
 				// score + (throw1*2 + throw2) + (throw1 + throw2)
-				System.out.println("1 - Score in the frame: "
-						+ (((scores[f][0] * 2 + scores[f][1]) + (scores[f][0] + scores[f][1]))));
+				// System.out.println("1 - Score in the frame: " + (((scores[f][0] * 2 +
+				// scores[f][1]) + (scores[f][0] + scores[f][1]))));
 				score += ((scores[f][0] * 2 + scores[f][1]) + (scores[f][0] + scores[f][1]));
 				if (f == 9 && ((scores[f][0] == 10) || (scores[f][0] + scores[f][1] == 10))) {
 					score += scores[f][2];
@@ -161,7 +209,8 @@ public class BowlingGame {
 				if (f == 9) {
 					score += scores[f][2];
 				}
-				System.out.println("2 - Score in the frame: " + (((scores[f][0] + scores[f][1]) * 2)));
+				// System.out.println("2 - Score in the frame: " + (((scores[f][0] +
+				// scores[f][1]) * 2)));
 			}
 			// spare last frame
 			else if ((scores[f - 1][0] != 10) && ((scores[f - 1][0] + scores[f - 1][1] == 10))) {// spare last frame
@@ -171,12 +220,14 @@ public class BowlingGame {
 				if (f == 9) {
 					score += scores[f][2];
 				}
-				System.out.println("3 - Score in the frame: " + ((scores[f][0] * 2) + scores[f][1]));
+				// System.out.println("3 - Score in the frame: " + ((scores[f][0] * 2) +
+				// scores[f][1]));
 
 			}
 			// regular score tracking
 			else {
-				System.out.println("4 - Score in the frame: " + ((scores[f][0] + scores[f][1])));
+				// System.out.println("4 - Score in the frame: " + ((scores[f][0] +
+				// scores[f][1])));
 				score += scores[f][0] + scores[f][1];
 			}
 
@@ -188,76 +239,112 @@ public class BowlingGame {
 
 			if (scores[f - 1][0] == 10) { // strike last frame
 				// score + (throw1 + throw2) * 2
-				System.out.println("5 - Score in the frame: " + (((scores[f][0] + scores[f][1]) * 2)));
+				// System.out.println("5 - Score in the frame: " + (((scores[f][0] +
+				// scores[f][1]) * 2)));
 				score += ((scores[f][0] + scores[f][1]) * 2);
 			}
 
 			else if ((scores[f - 1][0] != 10) && (scores[f - 1][0] + scores[f - 1][1] == 10)) {// spare last frame
 				// score + throw1 * 2 + throw 2
-				System.out.println("6 - Score in the frame: " + ((scores[f][0] * 2) + scores[f][1]));
+				// System.out.println("6 - Score in the frame: " + ((scores[f][0] * 2) +
+				// scores[f][1]));
 				score += (scores[f][0] * 2) + scores[f][1];
 
 			} else {
-				System.out.println("7 - Score in the frame: " + ((scores[f][0] + scores[f][1])));
+				// System.out.println("7 - Score in the frame: " + ((scores[f][0] +
+				// scores[f][1])));
 				score += scores[f][0] + scores[f][1];
 			}
 
 		}
 
 		else { // first frame
-			System.out.println("8 - Score in the frame: " + ((scores[f][0] + scores[f][1])));
+				// System.out.println("8 - Score in the frame: " + ((scores[f][0] +
+				// scores[f][1])));
 			score += scores[f][0] + scores[f][1];
 		}
 	}
 
+	/**
+	 * Purpose: The computeScore method computes the bowler's score after each the
+	 * game has finished. It is similar to the computeScoreFrame method except it
+	 * computes the score once all of the frames have been finished.
+	 * 
+	 * Scoring Rules:
+	 * 
+	 * Strikes: 10 points in the current frame plus score from next two shots
+	 * 
+	 * Spares: 10 points in the current frame plus score from next shot only
+	 * 
+	 * For Example: If a strike is bowled in frame 1, 10 points are awarded. Then,
+	 * if the next two shots are 2 and 5, 7 points are added to frame 1, and 7
+	 * points are added to the resulting score from frame 1 for a total score of 24
+	 * points after frame 2.
+	 * 
+	 * If a spare is bowled in frame 1, 10 points are awarded. Then, if the next
+	 * shot is a 2, 2 points are added to frame 1, and 2 points are added to the
+	 * resulting score from frame 1 for a total score of 14 points after frame 2.
+	 * 
+	 * @param nothing
+	 * @return nothing
+	 * 
+	 */
 	public void computeScore() {
 		int scoreCheck = 0;
 
 		for (int i = 0; i < maxNumFrames; i++) {
 
-			// if current frame was a strike, then add the scores from the next 2 rolls to
-			// the current frame
-			// if current frame was a spare, then add the score from the next 1 frame to the
-			// current frame
-			// otherwise just go to the next frame and add the score like usual
 			if (i < 9) {
 				if (scores[i][0] == 10) { // strike this frame
-					frameScores[i] += 10;
+
+					scoreCheck += 10;
 					if (scores[i + 1][0] == 10) {// because of strike, check 1 of 2 roll for another strike
-						frameScores[i] += 10;
+
+						scoreCheck += 10;
 						if (i == 8 && scores[i + 1][1] == 10) {
 							// frame 9, check roll 2 of frame 10
-							frameScores[i] += 10;
+
+							scoreCheck += 10;
 						} else {
 							if (scores[i + 2][0] == 10) {// because of strike, check 2 of 2 roll for another strike
-								frameScores[i] += 10;
+
+								scoreCheck += 10;
 							} else {
-								frameScores[i] += scores[i + 2][0]; // no strike on roll 2 of 2, just add the pins
+
+								scoreCheck += scores[i + 2][0]; // no strike on roll 2 of 2, just add the pins
 							}
 
 						}
 
 					} // need else here for adding regular score
 					else {
-						frameScores[i] += (scores[i + 1][0] + scores[i + 1][1]);
+
+						scoreCheck += (scores[i + 1][0] + scores[i + 1][1]);
 					}
 				} else if ((scores[i][0] != 10) && (scores[i][0] + scores[i][1] == 10)) { // spare this frame
-					frameScores[i] += (scores[i][0] + scores[i][1] + scores[i + 1][0]); // add score from next 1 roll
+
+					scoreCheck += (scores[i][0] + scores[i][1] + scores[i + 1][0]); // add score from next 1 roll
 				} else {
-					frameScores[i] += (scores[i][0] + scores[i][1]);
+
+					scoreCheck += (scores[i][0] + scores[i][1]);
 				}
 			} else if (i == 9) {
-				frameScores[i] += (scores[i][0] + scores[i][1] + scores[i][2]);
+
+				scoreCheck += (scores[i][0] + scores[i][1] + scores[i][2]);
 			}
 		}
-		for (int j = 0; j < frameScores.length; j++) {
-			scoreCheck += frameScores[j];
-			System.out.println(scoreCheck);
-		}
+
 		JOptionPane.showMessageDialog(null, "The bowler's verified score is: " + scoreCheck);
 
 	}
 
+	/**
+	 * Purpose: The getScore method is the getter method for the score variable.
+	 * 
+	 * @param nothing
+	 * @return an int representing the bowler's current score;
+	 * 
+	 */
 	public int getScore() {
 		return score;
 	}
--- workspace/Lab1_Williams/src/BowlingGameDriver.java
+++ workspace/Lab1_Williams/src/BowlingGameDriver.java
@@ -1,40 +1,59 @@
-
+/**
+ * Purpose: The BowlingGameDriver program is a driver class to execute
+ * the BowlingGame program. This class contains the main method for the
+ * program. Driver class provided by Ivan Temesvari.
+ * 
+ * Constructors: None
+ * 
+ * Methods: main
+ * 
+ * @author Ivan Temesvari, Robert Williams
+ * @since 2018-01-27
+ * 
+ */
 
 import javax.swing.JOptionPane;
 
 public class BowlingGameDriver{
+	/**
+	 * Purpose: The main method is the driver method for the BowlingGame program.
+	 * It creates a BowlingGame object, provides an option for user input for scores, 
+	 * and loops through the scores to calculate the score of the BowlingGame.
+	 * 
+	 * @param standard array of String args
+	 * @return nothing
+	 * 
+	 */
+	
 	public static void main(String[] args) {
 		String n = JOptionPane.showInputDialog("Please enter a name for a bowler: ");
 		BowlingGame g = new BowlingGame(n);
 		int scores1[][] = { {5, 5}, {3, 7}, {0, 0}, {4, 6}, {10, 0}, {1, 9}, {10, 0},
-							{8, 1}, {0, 10}, {10, 0, 10}
+							{8, 1}, {0, 10}, {10, 0, 10} //151 --151
 						  };
 		int scores2[][] = { {5, 5}, {3, 7}, {0, 0}, {4, 6}, {10, 0}, {1, 9}, {10, 0},
-							{8, 1}, {0, 10}, {0, 10, 10}
+							{8, 1}, {0, 10}, {0, 10, 10} //141 --141
 			  			  };
 		int scores3[][] = { {2, 6}, {10, 0}, {10, 0}, {5, 5}, {2, 0}, {7, 1}, {10, 0}, 
-						    {10, 0}, {5, 5}, {10, 3, 5} //total 158
-		//int scores3[][] = {{10, 3, 5} }; // {2, 6}, {10, 0}, {10, 0}, {5, 5}, {2, 0}, {7, 1}, {10, 0}, 
-		//				    {10, 0}, {5, 5}, {10, 3, 5} //total 158
-						  };
+						    {10, 0}, {5, 5}, {10, 3, 5} //total 158 --158
+		};
+		int scores4[][] = {{10, 0}, {10, 0}, {10, 0}, {10, 0}, {10, 0}, {10, 0}, {10, 0}, {10, 0}, {10, 0}, {10, 10, 10}}; //300
+		int scores5[][] = {{10, 0}, {10, 0}, {10, 0}, {10, 0}, {10, 0}, {10, 0}, {10, 0}, {10, 0}, {10, 0}, {5, 3}}; //261 -
+		int scores6[][] = {{10, 0}, {7, 3}, {7, 2}, {9, 1}, {10, 0}, {10, 0}, {10, 0}, {2, 3}, {6, 4}, {7, 3, 0}}; //165 -
 		int pins, i = 0, j;
 		while(g.getFrame() < 10){
 			j = 0;
 			do{
 				//pins = Integer.parseInt(JOptionPane.showInputDialog("Enter the pins knocked over for frame " + (g.getFrame()+1) + " :"));
 				
-				pins = scores1[i][j];
-				//System.out.println("i: "+i+" j: "+ j+" pins: "+pins);
-				
-				//System.out.println("i: "+i+" j: " + " pins: "+ pins +" frame: "+ (g.getFrame()));
-				//System.out.println(g.getFrame());
+				pins = scores2[i][j];
 				j++;
 			}while(g.shot(pins));
-			//System.out.println("another shot: "+ g.shot(pins));
 			i++;
-			//g.computeScoreFrame();
+			g.computeScoreFrame();
 			JOptionPane.showMessageDialog(null, g);	
 		}
+		g.computeScore();
 		JOptionPane.showMessageDialog(null, "The bowler's score is: " + g.getScore());	
 	}
 }
