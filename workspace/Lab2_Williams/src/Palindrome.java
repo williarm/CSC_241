181ad5d2dc66f6f0ee4a4233d385c87570cad52d
Bobby Williams <rwilliams@cardconnect.com>
2018-02-05 17:31:26 -0700

    Updates to program

--- workspace/Lab2_Williams/src/Palindrome.java
+++ workspace/Lab2_Williams/src/Palindrome.java
@@ -1,15 +1,29 @@
 public class Palindrome {
-	
-	public Palindrome() {
-		
-	}
-	
-	public void getPalindromicDate(Integer date) {
 
-		StackInterface<Integer> myStack;
-		myStack = new LinkedStack<Integer>();
-		myStack.push(date);
-		System.out.println(myStack.top());
+	public Palindrome() {
+
+	}
+
+	public void getPalindromicDate(Integer date) {
+		String month = date.toString().substring(0,2);
+		String day = date.toString().substring(2,4);
+		String year = date.toString().substring(4, 8);
+
+		System.out.println("In Month: "+ month);
+		System.out.println("In Day: "+ day);
+		System.out.println("In Year: " + year);
+		StackInterface<String> stack;
+		stack = new LinkedStack<String>();
+		stack.push(month);
+		stack.push(day);
+		stack.push(year);
 		
+		System.out.println("Top: "+ stack.top());
+		stack.pop();
+		System.out.println("Top: "+ stack.top());
+		stack.pop();
+		System.out.println("Top: "+ stack.top());
+		
+
 	}
 }
--- workspace/Lab2_Williams/src/PalindromeDriver.java
+++ workspace/Lab2_Williams/src/PalindromeDriver.java
@@ -4,9 +4,7 @@ public class PalindromeDriver {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Palindrome p = new Palindrome();
-		p.getPalindromicDate(12);
-		p.getPalindromicDate(02);
-		p.getPalindromicDate(2021);
+		p.getPalindromicDate(12022021);
 
 	}
 
