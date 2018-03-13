57b0e087c4e52eb653728e16678389777daca445
Bobby Williams <rwilliams@cardconnect.com>
2018-03-12 19:57:07 -0600

    Lab 5 updates

--- workspace/Lab5_Williams/src/Bag.java
+++ workspace/Lab5_Williams/src/Bag.java
@@ -75,10 +75,8 @@ public class Bag<T> extends ArrayCollection implements BagInterface {
 	}
 	
 	public String toString() {
-		Object o = new Object();
 		for (int i=0; i< this.numElements; i++) {
-			o = this.elements[i];
-			System.out.println(o);
+			System.out.println(this.elements[i]);
 		}
 		return null;
 	}
