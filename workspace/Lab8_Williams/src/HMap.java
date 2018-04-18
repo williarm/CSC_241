fbd50ad1e0e319853f1fb71a5bfc4a75812e2aa7
williarm <robert.m.williams2@gmail.com>
2018-04-18 07:02:29 -0600

    Add Lab 8 files

---
+++ workspace/Lab8_Williams/src/HMap.java
@@ -0,0 +1,261 @@
+
+//---------------------------------------------------------------------------
+// HMap.java                by Dale/Joyce/Weems                     Chapter 8
+//
+// Implements a map using an array-based hash table, linear probing collision 
+// resolution. 
+//
+// The remove operation is not supported. Invoking it will result in the 
+// unchecked UnsupportedOperationException being thrown.
+//
+// A map provides (K = key, V = value) pairs, mapping the key onto the value.
+// Keys are unique. Keys cannot be null.
+//
+// Methods throw IllegalArgumentException if passed a null key argument.
+//
+// Values can be null, so a null value returned by put or get does 
+// not necessarily mean that an entry did not exist.
+//---------------------------------------------------------------------------
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class HMap<K, V> implements MapInterface<K, V> {
+	protected MapEntry[] map;
+
+	protected final int DEFCAP = 1000; // default capacity
+	protected final double DEFLOAD = 0.75; // default load
+
+	protected int origCap; // original capacity
+	protected int currCap; // current capacity
+	protected double load;
+
+	protected int numPairs = 0; // number of pairs in this map
+
+	public HMap() {
+		map = new MapEntry[DEFCAP];
+		origCap = DEFCAP;
+		currCap = DEFCAP;
+		load = DEFLOAD;
+	}
+
+	public HMap(int initCap, double initLoad) {
+		map = new MapEntry[initCap];
+		origCap = initCap;
+		currCap = initCap;
+		load = initLoad;
+	}
+
+	private void enlarge()
+	// Increments the capacity of the map by an amount
+	// equal to the original capacity.
+	{
+		// create a snapshot iterator of the map and save current size
+		Iterator<MapEntry<K, V>> i = iterator();
+		int count = numPairs;
+
+		// create the larger array and reset variables
+		map = new MapEntry[currCap + origCap];
+		currCap = currCap + origCap;
+		numPairs = 0;
+
+		// put the contents of the current map into the larger array
+		MapEntry entry;
+		for (int n = 1; n <= count; n++) {
+			entry = i.next();
+			this.put((K) entry.getKey(), (V) entry.getValue());
+		}
+	}
+
+	public V put(K k, V v)
+	// If an entry in this map with key k already exists then the value
+	// associated with that entry is replaced by value v and the original
+	// value is returned; otherwise, adds the (k, v) pair to the map and
+	// returns null.
+	{
+		if (k == null)
+			throw new IllegalArgumentException("Maps do not allow null keys.");
+
+		MapEntry<K, V> entry = new MapEntry<K, V>(k, v);
+
+		int location = Math.abs(k.hashCode()) % currCap;
+		while ((map[location] != null) && !(map[location].getKey().equals(k)))
+			location = (location + 1) % currCap;
+
+		if (map[location] == null) // k was not in map
+		{
+			map[location] = entry;
+			numPairs++;
+			if ((float) numPairs / currCap > load)
+				enlarge();
+			return null;
+		} else // k already in map
+		{
+			V temp = (V) map[location].getValue();
+			map[location] = entry;
+			return temp;
+		}
+	}
+
+	public V get(K k) throws NullPointerException
+	// If an entry in this map with a key k exists then the value associated
+	// with that entry is returned; otherwise null is returned.
+	{
+		if (k == null)
+			throw new IllegalArgumentException("Maps do not allow null keys.");
+
+		int location = Math.abs(k.hashCode()) % currCap;
+		while ((map[location] != null) && !(map[location].getKey().equals(k)))
+			location = (location + 1) % currCap;
+		try { // Updated: Ivan Temesvari with try catch block. 4/19/2017
+			return (V) map[location].getValue();
+		} catch (NullPointerException E) {
+			System.out.println("ID not found.");
+			return null;
+		}
+	}
+
+	public V remove(K k)
+	{
+		if (k == null) {
+			throw new IllegalArgumentException("Maps do not allow null keys");
+		}
+		else
+		{
+			if (contains(k)) {
+				K key = (K)"00000";
+				V value = (V)"00000";
+				MapEntry<K, V> entry = new MapEntry<K, V>(key, value);
+				map[location(k)] = entry;
+				System.out.println("Removed Value");
+			}else {
+				System.out.println("Key " + k.toString()+ " not found.");
+			}
+		}
+	
+		return null;
+	}
+
+	public boolean contains(K k)
+	// Returns true if an entry in this map with key k exists;
+	// Returns false otherwise.
+	{
+		if (k == null)
+			throw new IllegalArgumentException("Maps do not allow null keys.");
+
+		int location = Math.abs(k.hashCode()) % currCap;
+		while (map[location] != null)
+			if (map[location].getKey().equals(k))
+				return true;
+			else
+				location = (location + 1) % currCap;
+
+		// if get this far then no current entry is associated with k
+		return false;
+	}
+
+	public int location(K k)
+	// Returns the actual location index if an entry in this map with key k exists;
+	// Returns -1 otherwise.
+	// Ivan Temesvari: 4/19/2017
+	{
+		if (k == null)
+			throw new IllegalArgumentException("Maps do not allow null keys.");
+
+		int location = Math.abs(k.hashCode()) % currCap;
+		while (map[location] != null)
+			if (map[location].getKey().equals(k))
+				return location;
+			else
+				location = (location + 1) % currCap;
+
+		return -1;
+	}
+
+	public boolean isEmpty()
+	// Returns true if this map is empty; otherwise, returns false.
+	{
+		return (numPairs == 0);
+	}
+
+	public boolean isFull()
+	// Returns true if this map is full; otherwise, returns false.
+	{
+		return false; // An HMap is never full
+	}
+
+	public int size()
+	// Returns the number of entries in this map.
+	{
+		return numPairs;
+	}
+
+	private class MapIterator implements Iterator<MapEntry<K, V>>
+	// Provides a snapshot Iterator over this map.
+	// Remove is not supported and throws UnsupportedOperationException.
+	{
+		int listSize = size();
+		private MapEntry[] list = new MapEntry[listSize];
+		private int previousPos = -1; // previous position returned from list
+
+		public MapIterator() {
+			int next = -1;
+			for (int i = 0; i < listSize; i++) {
+				next++;
+				while (map[next] == null)
+					next++;
+				list[i] = map[next];
+			}
+		}
+
+		public boolean hasNext()
+		// Returns true if the iteration has more entries; otherwise returns false.
+		{
+			return (previousPos < (listSize - 1));
+		}
+
+		public MapEntry<K, V> next()
+		// Returns the next entry in the iteration.
+		// Throws NoSuchElementException - if the iteration has no more entries
+		{
+			if (!hasNext())
+				throw new IndexOutOfBoundsException("illegal invocation of next " + " in HMap iterator.\n");
+			previousPos++;
+			return list[previousPos];
+		}
+
+		public void remove()
+		// Throws UnsupportedOperationException.
+		// Not supported. Removal from snapshot iteration is meaningless.
+		{
+			throw new UnsupportedOperationException("Unsupported remove attempted on " + "HMap iterator.\n");
+		}
+	}
+
+	// Ivan Temesvari: 4/19/2017
+	public String toString() {
+		String s = "";
+		Iterator<MapEntry<K, V>> items = iterator();
+		int count = 0;
+		MapEntry<K, V> item;
+		while (items.hasNext()) {
+			item = items.next();
+			s += "First Choice: " + (((int) (item.key) % origCap) + "") + "\nLocation: "
+					+ (location(item.key) + "\n") + item + "\n";
+			count++;
+		}
+		return s;
+	}
+
+	public Iterator<MapEntry<K, V>> iterator()
+	// Returns a snapshot Iterator over this map.
+	// Remove is not supported and throws UnsupportedOperationException.
+
+	{
+		return new MapIterator();
+	}
+
+	public double loadRatio() {
+		return (double) size() / currCap;
+	}
+}
---
+++ workspace/Lab8_Williams/src/HMapDriver.java
@@ -0,0 +1,66 @@
+
+import java.util.Random;
+
+public class HMapDriver {
+	public static void main(String[] args) {
+		boolean result;
+
+		HMap<Integer, String> test;
+
+		final int SIZE = 10;
+		final int IDRANGE = 10001;
+		final int INITIALRANGE = 2;
+
+		test = new HMap<Integer, String>(SIZE, 0.75);
+
+		int randomID;
+		String employeeInitials;
+		final String letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		long t_in, t_out;
+
+		Random myRandomID = new Random();
+		Random randomIndex = new Random();
+
+		String randomInitials = "";
+
+		t_in = System.nanoTime();
+		test.put(100, "IT");
+		t_out = System.nanoTime();
+
+		//System.out.println("Total time for insertion of: " + 100 + ":" + "IT" + " is:" + (t_out - t_in));
+
+		// test.remove(100);
+		for (int i = 0; i < SIZE; i++) {
+			// compute randomID here:
+			randomID = myRandomID.nextInt(IDRANGE);
+
+			for (int j = 0; j < INITIALRANGE; j++)
+				randomInitials += letters.charAt(randomIndex.nextInt(letters.length()));
+
+			t_in = System.nanoTime();
+
+			test.put(i, randomInitials);
+
+			t_out = System.nanoTime();
+
+			System.out.println(
+					"Total time for insertion of: " + randomID + ":" + randomInitials + " is: " + (t_out - t_in));
+			System.out.println("Load Ratio: "+ test.loadRatio());
+
+			System.out.println(test);
+
+			randomInitials = "";
+		}
+		
+		for (int i = 0; i < test.size(); i++) {
+			
+			test.remove(i);
+		}
+		
+for (int i = 0; i < test.size(); i++) {
+			
+			test.remove(i);
+		}
+System.out.println("Size: "+ test.size());
+	}
+}
